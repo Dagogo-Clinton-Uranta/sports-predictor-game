a7f8dd6409e59c69fbc640743c4f9e138cb1647c
Dagogo Clinton Uranta <dagogouranta@gmail.com>
2024-01-15 15:02:54 +0100

    added environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,24 +5,16 @@ import "firebase/auth";
 
 
 
-/*const firebaseConfig = {
-  apiKey: "AIzaSyCdlxFQSkuSiCD80rJnuuOVLbfwdtyKPzs",
-  authDomain: "ibara-34497.firebaseapp.com",
-  projectId: "ibara-34497",
-  storageBucket: "ibara-34497.appspot.com",
-  messagingSenderId: "886817189981",
-  appId: "1:886817189981:web:f69ca549f78c1186cbddb4",
-  measurementId: "G-TNGY4Q8KYS"
-};*/
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJpI5BzRGAXMtuiltQiCfeyp-ROgiAZz0",
-  authDomain: "fantasy-prem-774d6.firebaseapp.com",
-  projectId: "fantasy-prem-774d6",
-  storageBucket: "fantasy-prem-774d6.appspot.com",
-  messagingSenderId: "597317324603",
-  appId: "1:597317324603:web:6174d968ad02d1b239711e",
-  measurementId: "G-Y4L8ZG75MZ"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 
